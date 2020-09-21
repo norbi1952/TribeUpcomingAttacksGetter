3d6b245aefabf4ed32a37439d5259d4594831f4a
norbi1952 <norbi1952@gmail.com>
2020-09-22 00:00:31 +0200

    Update TribeUpcomingAttacksGetter.js

--- TribeUpcomingAttacksGetter.js
+++ TribeUpcomingAttacksGetter.js
@@ -1,44 +1,45 @@
 // ==UserScript==
-// @name			TribeUpcomingAttacksGetter
-// @version			0.1.1
-// @author			szelbi
-// @website			https://szelbi.ovh/
-// @match			*://*.plemiona.pl/*
-// @grant			none
-// @updateURL		https://raw.githubusercontent.com/norbi1952/TribeUpcomingAttacksGetter/master/TribeUpcomingAttacksGetter.js
+// @name          TribeUpcomingAttacksGetter
+// @version       0.2
+// @author        szelbi
+// @website       https://szelbi.ovh/
+// @match         *://*.plemiona.pl/*
+// @grant         none
+// @updateURL     https://raw.githubusercontent.com/norbi1952/TribeUpcomingAttacksGetter/master/TribeUpcomingAttacksGetter.js
 // @downloadURL		https://raw.githubusercontent.com/norbi1952/TribeUpcomingAttacksGetter/master/TribeUpcomingAttacksGetter.js
 // ==/UserScript==
 
+(function () {
+  "use strict";
 
-(function() {
-	"use strict";
+  var url = window.location.href;
 
-	var url = window.location.href;
+  if (
+    !url.includes("screen=ally&mode=members_troops") &&
+    !url.includes("screen=ally&mode=members_defense")
+  ) {
+    sessionStorage.removeItem("selectOptions");
+    sessionStorage.removeItem("playersArray");
+    return;
+  }
 
-	if(!url.includes("screen=ally&mode=members_troops") && !url.includes("screen=ally&mode=members_defense")) {
-		if (sessionStorage.getItem("selectOptions")) {
-			sessionStorage.removeItem("selectOptions");
-		}
-		if (sessionStorage.getItem("villageDictionary")) {
-			sessionStorage.removeItem("villageDictionary");
-		}
-		return;
-	}
+  var selectElement;
 
-	document.onreadystatechange = () => {
-		if (document.readyState === "complete") {
-			getData();
+  document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      selectElement = document.querySelector("form > select[name='player_id']");
+      getData();
 
-			let allyContent = document.getElementById("ally_content");
+      let getTroopsButton = document.createElement("button");
+      getTroopsButton.setAttribute("type", "button");
+      getTroopsButton.setAttribute("class", "btn btn-default float_right");
+      getTroopsButton.addEventListener("click", buttonOnClick);
+      getTroopsButton.innerHTML = "Pobierz dane o atakach";
 
-			let getTroopsButton = document.createElement("button");
-			getTroopsButton.setAttribute("type", "button");
-			getTroopsButton.setAttribute("class", "btn btn-default float_right");
-			getTroopsButton.addEventListener("click", buttonOnClick);
-			getTroopsButton.innerHTML = "Pobierz dane o atakach";
-			allyContent.insertBefore(getTroopsButton, allyContent.firstChild);
+      let allyContent = document.getElementById("ally_content");
+      allyContent.insertBefore(getTroopsButton, allyContent.firstChild);
 
-			let modalCSS = `
+      let modalCSS = `
 				.modal {
 					display: none;
 					position: fixed;
@@ -51,20 +52,21 @@
 					overflow: auto;
 				}
 				.modal-content {
-					background-color: #fefefe;
-					margin: auto;padding: 20px;
-					border: 1px solid #888;
-					width: 80%;
+          background-color: #ead5aa;
+          margin: auto;
+          padding: 20px;
+					border: 1px solid #7d510f;
+					width: 40%;
 				}
 				.close {
-				  color: #aaaaaa;
+				  color: #603000;
 				  float: right;
 				  font-size: 28px;
 				  font-weight: bold;
 				}
 				.close:hover,
 				.close:focus {
-				  color: #000;
+				  color: #803000;
 				  text-decoration: none;
 				  cursor: pointer;
 				}
@@ -73,133 +75,217 @@
 					height: 30em;
 					resize: none;
 				}
-			`
+			`;
+      addCSS(modalCSS);
 
-			addCSS(modalCSS);
+      let modalDiv = document.createElement("div");
+      modalDiv.setAttribute("id", "js-modal");
+      modalDiv.setAttribute("class", "modal");
+      modalDiv.innerHTML =
+        '<div class="modal-content"><span class="close" id="js-close">&times;</span><p><textarea class="textarea" id="js-text-box"></textarea></p></div>';
+      allyContent.parentNode.insertBefore(modalDiv, allyContent.nextSibling);
 
-			let modalDiv = document.createElement("div");
-			modalDiv.setAttribute("id", "atg-modal");
-			modalDiv.setAttribute("class", "modal");
+      document.getElementById("js-close").onclick = closeModalOnClick;
 
-			modalDiv.innerHTML = "<div class=\"modal-content\"><span class=\"close\" id=\"atg-close\">&times;</span><p><textarea class=\"textarea\" id=\"atg-text-box\"></textarea></p></div>";
+      if (typeof Storage !== "undefined") {
+        let sessionStorageItem;
+        if ((sessionStorageItem = sessionStorage.getItem("selectOptions"))) {
+          disableButton(getTroopsButton);
 
-			allyContent.parentNode.insertBefore(modalDiv, allyContent.nextSibling);
+          let selectOptions = JSON.parse(sessionStorageItem);
+          getNextPlayerTroops(selectOptions);
+        }
+      } else {
+        disableButton(getTroopsButton);
+        alert(
+          'Twoja przeglądarka nie wspiera "Web Storage". Zaktualizuj ją do najnowszej wersji.'
+        );
+      }
+    }
+  };
 
-			document.getElementById("atg-close").onclick = closeModalOnClick;
+  function addCSS(css) {
+    let style = document.createElement("style");
+    style.innerHTML = css;
+    document.head.appendChild(style);
+  }
 
-			if (typeof(Storage) !== "undefined") {
-				if (sessionStorage.getItem("selectOptions")) {
-					disableButton(getTroopsButton);
+  function closeModalOnClick() {
+    document.getElementById("js-modal").style.display = "none";
+  }
 
-					let selectOptions = JSON.parse(sessionStorage.getItem("selectOptions"));
-					getNextPlayerTroops(selectOptions);
-				}
-			} else {
-				disableButton(getTroopsButton);
-				alert("Twoja przeglądarka nie wspiera \"Web Storage\". Zaktualizuj ją do najnowszej wersji.");
-			}
-		}
-	};
+  function disableButton(button) {
+    button.classList.add("btn-disabled");
+    button.disabled = true;
+  }
 
-	function addCSS(css) {
-		let style = document.createElement("style");
-		style.innerHTML = css;
-		document.head.appendChild(style);
-	}
+  function buttonOnClick() {
+    disableButton(this);
 
-	function closeModalOnClick() {
-		document.getElementById("atg-modal").style.display = "none";
-	}
+    if (selectElement) {
+      let selectOptions = [];
+      for (const option of selectElement.options) {
+        if (!option.hidden && !option.disabled) {
+          selectOptions.push(option.value);
+        }
+      }
+      sessionStorage.setItem("selectOptions", JSON.stringify(selectOptions));
 
-	function disableButton(button) {
-		button.classList.add('btn-disabled');
-		button.disabled = true;
-	}
+      getNextPlayerTroops(selectOptions);
+    }
+  }
 
-	function buttonOnClick() {
-		disableButton(this);
-		
-		let selectElement = document.querySelector("form > select[name='player_id']");
-		if(selectElement) {
-			let selectOptions = [];
+  function getNextPlayerTroops(selectOptions) {
+    if (selectOptions.length) {
+      if (selectElement) {
+        selectElement.value = selectOptions[0];
+        selectElement.form.submit();
 
-			for (let i = 0; i < selectElement.options.length; i++) {
-				let option = selectElement.options[i];
+        selectOptions.shift();
 
-				if(!option.hidden && !option.disabled) {
-					selectOptions.push(option.value);
-				}
-			}
-			sessionStorage.setItem("selectOptions", JSON.stringify(selectOptions));
+        sessionStorage.setItem("selectOptions", JSON.stringify(selectOptions));
+      }
+    } else {
+      sessionStorage.removeItem("selectOptions");
 
-			getNextPlayerTroops(selectOptions);
-		}
-	}
+      let text = "";
+      let sessionStorageItem;
+      if ((sessionStorageItem = sessionStorage.getItem("playersArray"))) {
+        let parsedJSON = JSON.parse(sessionStorageItem);
+        let playersArray = parsedJSON.map((e1) => {
+          let player = new Player(e1.name);
+          player.villages = e1.villages.map(
+            (e2) => new Village(e2.coords, e2.attacksIncoming)
+          );
+          return player;
+        });
 
-	function getNextPlayerTroops(selectOptions) {
-		if(selectOptions.length) {
-			let selectElement = document.querySelector("form > select[name='player_id']");
-			if(selectElement) {
-				selectElement.value = selectOptions[0];
-				selectElement.form.submit();
+        for (const player of playersArray) {
+          text += `${player.name}\n`;
+          for (const village of player.villages) {
+            text += `${village.coords} - ${village.attacksIncoming}\n`;
+          }
+          text += "\n";
+        }
 
-				selectOptions.shift();
+        sessionStorage.removeItem("playersArray");
+      }
 
-				sessionStorage.setItem("selectOptions", JSON.stringify(selectOptions));
-			}
-		} else {
-			sessionStorage.removeItem("selectOptions");
+      if (!text) {
+        text = "Brak ataków na graczy.";
+      }
+      document.getElementById("js-text-box").value = text.trim();
+      document.getElementById("js-modal").style.display = "block";
+    }
+  }
 
-			if (sessionStorage.getItem("villageDictionary")) {
-				let villageDictionary = JSON.parse(sessionStorage.getItem("villageDictionary"));
+  function getData() {
+    const tableRows = document.querySelectorAll(
+      ".table-responsive > .vis > tbody > tr"
+    );
 
-				let text = "";
-				for(let coords in villageDictionary) {
-					let attacksIncoming = villageDictionary[coords];
-					text = `${text}${coords} - ${attacksIncoming}\n`;
-				}
-				let modal = document.getElementById("atg-modal");
-				modal.style.display = "block";
+    let step = 1;
+    if (url.includes("screen=ally&mode=members_defense")) {
+      step = 2;
+    }
 
-				if(!text) {
-					text = "Brak ataków na graczy.";
-				}
-				document.getElementById("atg-text-box").value = text;
+    if (tableRows && selectElement) {
+      let sessionStorageItem;
+      let playersArray = [];
+      if ((sessionStorageItem = sessionStorage.getItem("playersArray"))) {
+        playersArray = JSON.parse(sessionStorageItem);
+      }
 
-				sessionStorage.removeItem("villageDictionary");
-			}
-		}
-	}
+      let player = new Player(
+        selectElement.options[selectElement.selectedIndex].text
+      );
 
-	function getData() {
-		let tableRows = document.querySelectorAll('.table-responsive > .vis > tbody > tr');
+      for (let i = 1; i < tableRows.length; i += step) {
+        const rowData = tableRows[i].querySelectorAll("td");
+        const lastCol = rowData[rowData.length - 1];
 
-		let step = 1;
-		if(url.includes("screen=ally&mode=members_defense")) {
-			step = 2;
-		}
+        if (!lastCol.classList.contains("hidden")) {
+          let villageName = rowData[0].innerText;
+          let result = villageName.match(
+            /(?:\()(\d{3}\|\d{3})(?:\)\s\K\d{2})$/
+          );
+          let coords = result[1];
+          let attacksIncoming = lastCol.innerText;
+          player.addVillage(new Village(coords, parseInt(attacksIncoming)));
+        }
+      }
 
-		if(tableRows) {
-			let villageDictionary = {};
-			if (sessionStorage.getItem("villageDictionary")) {
-				villageDictionary = JSON.parse(sessionStorage.getItem("villageDictionary"));
-			}
+      if (player.villages.length) {
+        playersArray.push(player);
+        sessionStorage.setItem("playersArray", JSON.stringify(playersArray));
+      }
+    }
+  }
 
-			for (let i = 1; i < tableRows.length; i = i + step) {
-				let rowData = tableRows[i].querySelectorAll('td');
+  class Player {
+    constructor(name) {
+      this._name = name;
+      this._villages = [];
+    }
 
-				if(!rowData[rowData.length - 1].classList.contains("hidden")) {
-					let villageName = rowData[0].innerText;
-					let result = villageName.match(/(?:\()(\d{3}\|\d{3})(?:\)\s\K\d{2})$/);
-					let coords = result[1];
+    get name() {
+      return this._name;
+    }
 
-					let attacksIncoming = rowData[rowData.length - 1].innerText;
+    set name(name) {
+      this._name = name;
+    }
 
-					villageDictionary[coords] = attacksIncoming;
-				}
-			}
+    get villages() {
+      return this._villages;
+    }
 
-			sessionStorage.setItem("villageDictionary", JSON.stringify(villageDictionary));
-		}
-	}
+    set villages(villages) {
+      this._villages = villages;
+    }
+
+    addVillage(village) {
+      if (village instanceof Village) {
+        this._villages.push(village);
+      }
+    }
+
+    removeVillage(village) {
+      const index = this._villages.indexOf(village);
+      if (index > -1) {
+        this._villages.splice(index, 1);
+      }
+    }
+
+    toJSON() {
+      return { name: this._name, villages: this._villages };
+    }
+  }
+
+  class Village {
+    constructor(coords, attacksIncoming) {
+      this._coords = coords;
+      this._attacksIncoming = attacksIncoming;
+    }
+
+    get coords() {
+      return this._coords;
+    }
+
+    set coords(coords) {
+      this._coords = coords;
+    }
+
+    get attacksIncoming() {
+      return this._attacksIncoming;
+    }
+
+    set attacksIncoming(attacksIncoming) {
+      this._attacksIncoming = attacksIncoming;
+    }
+
+    toJSON() {
+      return { coords: this._coords, attacksIncoming: this._attacksIncoming };
+    }
+  }
 })();
